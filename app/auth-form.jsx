7ae428f89a98e00dcde8af5fddbe10fcbaa07301
CodeSandbox <support@codesandbox.io>
2023-09-21 22:50:49 +0000

    Sandbox snapshot commit

---
+++ app/auth-form.jsx
@@ -0,0 +1,24 @@
+"use client"
+import { Auth } from "@supabase/auth-ui-react"
+import { ThemeSupa } from "@supabase/auth-ui-shared"
+//import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { createClient } from '@supabase/supabase-js'
+
+export default function AuthForm() {
+//  const supabase = createClientComponentClient()
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  prcoess.envNEXT_PUBLIC_SUPABASE_ANON_KEY
+)
+  return (
+    <Auth
+      supabaseClient={supabase}
+      view="magic_link"
+      appearance={{ theme: ThemeSupa }}
+      theme="dark"
+      showLinks={false}
+      providers={[]}
+      redirectTo="http://localhost:3000/auth/callback"
+    />
+  )
+}
